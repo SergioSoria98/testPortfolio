459ddafd159859dbbf9d568d625e0c33445bd607
SergioSoria98 <descobic1998@gmail.com>
2022-07-18 14:24:14 +0200

    segunda prueba

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -3,19 +3,20 @@
 
     <head>
         <title>@yield('title', 'Aprendible')</title>
+    <style>
+        .active a {
+            color: red;
+            text-decoration: none;
+        }
+
+    </style>
+    
     </head>
 
     <body>
-        <nav>
-        <ul>
-            <li><a href="/">Home</a></li>
-            <li><a href="/about">About</a></li>
-            <li><a href="/portfolio">PortFolio</a></li>
-            <li><a href="/contact">Contact</a></li>
-        </ul>   
-        </nav>
+     
 
-
+       @include('partials.nav')
        @yield('content')
 
        
