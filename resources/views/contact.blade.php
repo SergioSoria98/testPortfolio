df2c61fd7a6e263a6b0b1c3efd09720f94adca46
SergioSoria98 <descobic1998@gmail.com>
2022-07-19 20:38:48 +0200

    Formulario errores(html en php)

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -5,14 +5,13 @@
 
 @section('content')
     <h1>Contact</h1>
-    @if($errors->any())
-        {{ $errors->first('name')}}
-    @endif
     
     <form method="POST" action="{{ route('contact')  }}">
         @csrf
         <input name="name" placeholder="Nombre..." value="" ><br>
+        {!! $errors->first('name','<small>:message</small>')!!}<br>
         <input type="email" name="email" placeholder="Email..."><br>
+        {{ $errors->first('email')}}<br>
         <input name="subject" placeholder="Asunto..." value="Asunto prueba"><br>
         <textarea name="content" placeholder="Mensaje...">Mensaje prueba</textarea><br>
         <button>Enviar</button>
